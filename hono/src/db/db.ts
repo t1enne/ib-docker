509d1c1067cdc0b9ada4fc8106f514b4a874b77a
t1enne <nasir.taov@gmail.com>
2025-11-29 09:13:19 +0100

    moved out db

--- hono/src/db/db.ts
+++ hono/src/db/db.ts
@@ -4,7 +4,7 @@ import path from "path";
 import type { DatabaseSchema } from "./types";
 import { BunSqliteDialect } from "kysely-bun-sqlite";
 
-const dbPath = path.join(process.cwd(), "db.sqlite");
+const dbPath = path.join(process.cwd(), "..", "data", "db.sqlite");
 const _db = new Database(dbPath);
 const dialect = new BunSqliteDialect({
   database: _db,

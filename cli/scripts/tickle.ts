0730b2b983742ca1f6c8f3bde91f20f8d70e01da
t1enne <nasir.taov@gmail.com>
2026-07-20 18:33:03 +0200

    feat: remove unused cli module

--- cli/scripts/tickle.ts
+++
@@ -1,6 +0,0 @@
-import { attemptAsync, invariant } from "es-toolkit";
-import { client } from "../src/shared";
-
-const [err, r] = await attemptAsync(() => client.post("tickle"));
-invariant(r, `${err}`);
-console.log(r.data);
